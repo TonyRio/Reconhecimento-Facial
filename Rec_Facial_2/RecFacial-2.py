3f5233828192169ddab7b9725f16324dac47f1b0
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-11-08 19:07:00 -0300

    Update RecFacial-2.py

--- Rec_Facial_2/RecFacial-2.py
+++ Rec_Facial_2/RecFacial-2.py
@@ -8,3 +8,23 @@ solucao_reconhecimento_rosto = mp.solutions.face_detection
 reconhecedor_rostos = solucao_reconhecimento_rosto.FaceDetection()
 desenho = mp.solutions.drawing_utils
 
+while True:
+    #ler informacoes da webcam
+    verificador, frame = webcam.read()
+
+    if not verificador:
+        break
+    #reconhecer os rostos na imagem
+    lista_rostos = reconhecedor_rostos.process(frame)
+
+    if lista_rostos.detections:
+    # desenhar os rostos na imagem
+        for rosto in lista_rostos.detections:
+            desenho.draw_detection(frame, rosto)
+    cv2.imshow("Rostos na Webcam", frame)
+
+    #Quando apertar 'Esc' ele parar o Loop
+    if cv2.waitKey(5) == 27:
+        break
+
+webcam.release()
