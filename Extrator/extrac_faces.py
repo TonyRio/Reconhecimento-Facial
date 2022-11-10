92b49211b04650356d125d315f4507ca157920c3
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-11-10 01:54:29 -0300

    Update extrac_faces.py

--- Extrator/extrac_faces.py
+++ Extrator/extrac_faces.py
@@ -5,6 +5,7 @@ from os.path import isdir
 from numpy import asarray
 
 detector = MTCNN()
+
 def extrair_face(arquivo, size=(160,160)):
 
     img =  Image.open(arquivo) # Caminho completo
@@ -24,3 +25,26 @@ def extrair_face(arquivo, size=(160,160)):
     image = image.resize(size)
 
     return image
+
+
+def load_fotos(directory_src, directory_target):
+
+    print(directory_src)
+    print(directory_target)
+
+
+def load_dir(directoy_src, directory_target):
+    for subdir in listdir(directoy_src):
+
+        path = directoy_src + subdir + "\\"
+
+        path_tg = directory_target + subdir + "\\"
+
+        if not isdir(path):
+            continue
+
+        load_fotos(path, path_tg)
+
+if __name__ == '__main__':
+    load_dir("C:\\programas - TI\\DATA_SETS\\fotos-Tonye Cintia",
+             "C:\\programas - TI\\DATA_SETS\\Faces_family")
