29d7560fa77102680cc68f757933fe30e75169ff
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-11-10 01:27:38 -0300

    Update extrac_faces.py

--- Extrator/extrac_faces.py
+++ Extrator/extrac_faces.py
@@ -2,3 +2,18 @@ from  mtcnn import MTCNN
 from PIL import Image
 from os import listdir
 from os.path import isdir
+from numpy import asarray
+
+detector = MTCNN()
+def extrair_face(arquivo, size=(160,160)):
+
+    img =  Image.open(arquivo) # Caminho completo
+
+    img =  img.convert('RGB') # Converter em RGB
+
+    array = asarray(img)
+
+    results = detector.detect_faces(array)
+    x1, y1 , width, height = results[0] ['box']
+
+    x2, y2 = x1 + width, y1 + height
