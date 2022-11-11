c9bd9e39b72df215b8f72273875eb9d7c400be86
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-11-11 15:29:06 -0300

    Create Embed_images.py

---
+++ Embedding/Embed_images.py
@@ -0,0 +1,26 @@
+from  mtcnn import MTCNN
+from PIL import Image
+from os import listdir
+from os.path import isdir
+from numpy import asarray, expand_dims
+import numpy as np
+
+def load_face(filename): #, required_size =(160,160)):
+    #carregando arquivo
+    image = Image.open(filename)
+
+    #converter par rgb
+
+    image = image.convert("RGB")
+
+    return asarray(image)
+
+### CARREGANDO AS FACES DO DIRETORIO
+
+def load_faces(directory_src):
+
+    faces = list()
+# Iterando arquivos
+    for filename in listdir(directory_src):
+        path = directory_src + filename
+        load_face(path)
